e7149981d5723d8dee5c196c0a2c81b4b5f20272
amateishchuk <amateishchuk@gmail.com>
2017-08-08 11:08:41 +0300

    Implemented Logger (log4net)

--- Joost.Api/Controllers/HomeController.cs
+++ Joost.Api/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Joost.Api.Controllers
-{
+{    
+
 	public class HomeController : Controller
 	{
+        // static ILog log = LogManager.GetLogger(typeof(HomeController));
 		public ActionResult Index()
 		{
 			ViewBag.Title = "Home Page";
--- Joost.Api/Global.asax.cs
+++ Joost.Api/Global.asax.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net.Config;
 
 namespace Joost.Api
 {
-	public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : System.Web.HttpApplication
 	{
 		protected void Application_Start()
 		{
+            // XmlConfigurator.Configure();
 			AreaRegistration.RegisterAllAreas();
 			GlobalConfiguration.Configure(WebApiConfig.Register);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
