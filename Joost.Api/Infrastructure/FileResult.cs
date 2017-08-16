549fa83e82d46a0dde446791fdb4b7b5fad7cb6e
Andrew <andrew.bulkovskiy@gmail.com>
2017-08-16 21:05:10 +0300

    Added AvatarsController, Avatars folder, updated Seed method

---
+++ Joost.Api/Infrastructure/FileResult.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Joost.Api.Infrastructure
+{
+    public class FileResult : IHttpActionResult
+    {
+        private readonly string filePath;
+        private readonly string contentType;
+
+        public FileResult(string filePath, string contentType = null)
+        {
+            this.filePath = filePath;
+            this.contentType = contentType;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StreamContent(File.OpenRead(filePath))
+                };
+
+                var contentType = this.contentType ?? MimeMapping.GetMimeMapping(Path.GetExtension(filePath));
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue(contentType);
+
+                return response;
+            }, cancellationToken);
+        }
+    }
+}
--- Joost.DbAccess/Migrations/Configuration.cs
+++ Joost.DbAccess/Migrations/Configuration.cs
@@ -16,12 +16,12 @@
         {
             var users = new List<User>
             {
-                new User { Email="andrewbulkovskiy@gmail.com", Password="password", FirstName="Andrew", LastName = "Bulkovskiy" , State = UserState.Online },
-                new User { Email="amateishchuk@gmail.com", Password="password", FirstName="Andrii", LastName = "Mateishchuk" , State = UserState.Online },
-                new User { Email="artyom@gmail.com", Password="password", FirstName="Artyom", LastName = "Moiseenko" , State = UserState.Online },
-                new User { Email="diana@gmail.com", Password="password", FirstName="Diana", LastName = "Kolisnichenko" , State = UserState.Online },
-                new User { Email="raingeragon@gmail.com", Password="password", FirstName="Ilya", LastName = "Khomenko" , State = UserState.Online },
-                new User { Email="legodov@gmail.com", Password="password", FirstName="Oleh", LastName = "Dovhan" , State = UserState.Online },
+                new User { Email="andrewbulkovskiy@gmail.com", Password="password", FirstName="Andrew", LastName = "Bulkovskiy" , State = UserState.Online, Avatar = "1_avatar.jpg", IsActived = true },
+                new User { Email="amateishchuk@gmail.com", Password="password", FirstName="Andrii", LastName = "Mateishchuk" , State = UserState.Online, IsActived = true },
+                new User { Email="artyom@gmail.com", Password="password", FirstName="Artyom", LastName = "Moiseenko" , State = UserState.Online, IsActived = true },
+                new User { Email="diana@gmail.com", Password="password", FirstName="Diana", LastName = "Kolisnichenko" , State = UserState.Online, IsActived = true },
+                new User { Email="raingeragon@gmail.com", Password="password", FirstName="Ilya", LastName = "Khomenko" , State = UserState.Online, IsActived = true },
+                new User { Email="legodov@gmail.com", Password="password", FirstName="Oleh", LastName = "Dovhan" , State = UserState.Online, IsActived = true },
                 new User { Email="straber@ukr.net", Password="password", FirstName="Oleksandr", LastName = "Truba" , State = UserState.Online, IsActived = true },
                 new User { Email="daria@gmail.com", Password="password", FirstName="Darina", LastName = "Korotkih" , State = UserState.Online, IsActived = true },
                 new User { Email="rrational@gmail.com", Password="password", FirstName="Vasyl", LastName = "Barna" , State = UserState.Online, IsActived = true },
