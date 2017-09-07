b912ccb7c6242367db88fd65c2041ef7466e4d39
raingeragon <raingeragon@mail.ru>
2017-09-07 21:15:48 +0300

    user status

--- Joost.Client/src/app/components/menu-search/menu-search.component.ts
+++ Joost.Client/src/app/components/menu-search/menu-search.component.ts
@@ -213,6 +213,11 @@ export class MenuSearchComponent implements OnInit{
 		    }
 		}
 	}
+	contactStatus(id:number):string{
+		let state = this.contactList.filter(x=>x.ContactId==id)[0];
+		if(state)
+			switch(state)
+	}
 
 	ContainsCyrillicChar(str) {
 		return /[\u0400-\u04FF]/.test(str);
